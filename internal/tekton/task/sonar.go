583895c2823d47a19eaad8276a98e689242d8c24
David ALEXANDRE <david.alexandre@w6d.io>
2021-01-16 23:45:45 +0100

    update licence

--- internal/tekton/task/sonar.go
+++ internal/tekton/task/sonar.go
@@ -1,15 +1,17 @@
 /*
-Copyright 2020 WILDCARD SA.
+Copyright 2020 WILDCARD
 
-Licensed under the WILDCARD SA License, Version 1.0 (the "License");
-WILDCARD SA is register in french corporation.
-You may not use this file except in compliance with the License.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.w6d.io/licenses/LICENSE-1.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is prohibited.
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 Created on 29/11/2020
 */
 
